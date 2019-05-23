2b5e04377a553c69b70694bb95b86efdc4223e37
Micah <micahincorporated@gmail.com>
2019-05-23 13:59:03 -0500

    Add files via upload

---
+++ network/unknown_DNS.bat
@@ -0,0 +1,35 @@
+ECHO OFF
+ECHO script started
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------  
+ECHO admin permissions secured.  
+PAUSE
+netsh interface ipv4 show config
+netsh interface ip set dns "Wi-Fi Adapter" static 8.8.8.8
+ECHO DNS servers have been set to 8.8.8.8 and 8.8.4.4
